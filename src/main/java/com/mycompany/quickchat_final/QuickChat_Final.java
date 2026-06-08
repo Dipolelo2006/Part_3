e89567118b708e44a9efc856b281519f5faaa1ba
Student <Student@LabVM2047644.ju4k3udt5hzunlolye3o2piqrc.jnbx.internal.cloudapp.net>
2026-06-09 00:44:16 +0200

    Created part 3 project

---
+++ src/main/java/com/mycompany/quickchat_final/QuickChat_Final.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quickchat_final;
+
+/**
+ *
+ * @author Student
+ */
+public class QuickChat_Final {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
