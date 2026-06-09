466fcc114d18f51d2c39e9b8af348cac11fd5c81
Student <Student@LabVM2047644.ju4k3udt5hzunlolye3o2piqrc.jnbx.internal.cloudapp.net>
2026-06-09 20:16:04 +0200

    Created QuickChat_Final.java

--- src/main/java/com/mycompany/quickchat_final/QuickChat_Final.java
+++ src/main/java/com/mycompany/quickchat_final/QuickChat_Final.java
@@ -4,13 +4,37 @@
 
 package com.mycompany.quickchat_final;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 /**
- *
+ * Combines Registration_And_Login, Sending Messages and stored messages functionality.
  * @author Student
  */
 public class QuickChat_Final {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner (System.in);
+        System.out.println("Welcome to QuickChat.");
+        
+        // Part 1: Login
+        boolean isLoggedIn = login(input);
+        if (!isLoggedIn){
+            System.out.println("Login failed. Exiting.");
+            input.close();
+            return;
+        }
+        // part 3: Populate test data (no hard-coding in logic)
+        populateTestData();
+        
+        boolean running = true;
+        while (running){
+            System.out.println("\n====QuickChat Main Menu====");
+            System.out.println("1. Send Messages");
+            System.out.println("2. Show recently sent messages");
+            System.out.println("3. Quit");
+            System.out.println("4. Stored Messages");
+            System.out.println("Enter your choice:");
+        }
     }
 }
